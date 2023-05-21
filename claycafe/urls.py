d501a6fe94504bd0a1608a6857c03986ca0afc46
Dmytro Martynenko <dmytromartynenko1@gmail.com>
2023-05-21 13:30:06 +0000

    Create test view and url to display products info

--- claycafe/urls.py
+++ claycafe/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
+    path('products/', include('product.urls'))
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- product/views.py
+++ product/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from .models import Category, Product
 # Create your views here.
+
+def all_products(request):
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'product/product.html', context)
