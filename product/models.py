a1d668e27b49d4513db086078f65adb71b1011ad
Dmytro Martynenko <dmytromartynenko1@gmail.com>
2023-05-21 22:22:41 +0000

    Add test products, customize and style products template

--- product/models.py
+++ product/models.py
@@ -17,6 +17,8 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField()
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    rating = rating = models.DecimalField(max_digits=6, decimal_places=2, null=True,
+                                          blank=True)
 
     def __str__(self):
         return self.name
