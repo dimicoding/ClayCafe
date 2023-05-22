3f833746931981ebe88b81d293a1f2cc794ada5c
Dmytro Martynenko <dmytromartynenko1@gmail.com>
2023-05-22 14:33:29 +0000

    Customize product view template and change footers map

--- product/views.py
+++ product/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
 # Create your views here.
 
 
 def all_products(request):
+
+    """ Renders the view with all off the products available"""
 
     products = Product.objects.all().order_by('-id')
 
@@ -12,3 +14,16 @@ def all_products(request):
     }
 
     return render(request, 'product/product.html', context)
+
+
+def product_detail(request, product_id):
+
+    """ Renders the view of the specific product"""
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'product/product_detail.html', context)
