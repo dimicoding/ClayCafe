37858f10d92157294240e35f2426d2778ea1594e
Dmytro Martynenko <dmytromartynenko1@gmail.com>
2023-05-23 16:16:17 +0000

    Customize sorting and filtering of products

--- product/views.py
+++ product/views.py
@@ -14,6 +14,17 @@ def all_products(request):
     categories = None
 
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
 
         # python documentation for urllib.parse and unquote https://docs.python.org/3/library/urllib.parse.html
         if 'category' in request.GET:
@@ -32,10 +43,13 @@ def all_products(request):
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'products': products,
         'search_term': query,
         'current_categories': categories,
+        'current_sorting': current_sorting
     }
 
     return render(request, 'product/products.html', context)
