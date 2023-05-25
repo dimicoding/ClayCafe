74cf462c288fc595377c9b31ab3b91aca7b75a2f
Dmytro Martynenko <dmytromartynenko1@gmail.com>
2023-05-25 09:28:08 +0000

    Add view to handle the quantity update in the bag

--- bag/views.py
+++ bag/views.py
@@ -21,3 +21,18 @@ def add_to_bag(request, item_id):
 
     request.session['bag'] = bag
     return redirect(redirect_url)
+
+def adjust_bag(request, item_id):
+    """Adjust the quantity of the specified product to the specified amount"""
+
+    product = get_object_or_404(Product, pk=item_id)
+    quantity = int(request.POST.get('quantity'))
+    bag = request.session.get('bag', {})
+
+    if quantity > 0:
+        bag[item_id] = quantity
+    else:
+        bag.pop(item_id)
+        
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
