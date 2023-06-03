acd744d5ad53e58175335aac0bffb606f40f27dd
Dmytro Martynenko <dmytromartynenko1@gmail.com>
2023-06-03 14:31:06 +0000

    Generate blog posts and display them on the blog.html template

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from .views import HomeView
 
 
 urlpatterns = [
-    path('', views.blog, name="blog"),
+    # path('', views.blog, name="blog"),
+    path("", HomeView.as_view(), name="blog"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView
+from .models import Blog, Comment
 # Create your views here.
 
 def blog(request):
+
     return render(request,"blog/blog.html", {})
+
+
+class HomeView(ListView):
+    """
+    Accessible from "blog" navbar, displays all blog posts
+    """
+
+    model = Blog
+    template_name = "blog/blog.html"
+    ordering = ["-created_blog"]
+
+    # Dropdown menu list
+    # def get_context_data(self, *args, **kwargs):
+    #     cat_list = Category.objects.all()
+    #     context = super(HomeView, self).get_context_data(*args, **kwargs)
+    #     context["cat_list"] = cat_list
+    #     return context
