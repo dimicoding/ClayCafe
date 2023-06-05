7c97c32dc95735977e5d899bc3dea200b8231e47
Dmytro Martynenko <dmytromartynenko1@gmail.com>
2023-06-05 14:47:05 +0000

    prefill the form with any info the user maintains in their profile

--- checkout/views.py
+++ checkout/views.py
@@ -92,8 +92,23 @@ def checkout(request):
             amount=stripe_total,
             currency=settings.STRIPE_CURRENCY,
         )
-
-        order_form = OrderForm()
+        # Attempt to prefill the form with any info the user maintains in their profile	
+        if request.user.is_authenticated:	
+            try:	
+                profile = UserProfile.objects.get(user=request.user)	
+                order_form = OrderForm(initial={	
+                    'full_name': profile.user.get_full_name(),	
+                    'email': profile.user.email,	
+                    'phone_number': profile.default_phone_number,	
+                    'postcode': profile.default_postcode,	
+                    'city': profile.default_city,	
+                    'street_address1': profile.default_address1,	
+                    'street_address2': profile.default_address2,	
+                })	
+            except UserProfile.DoesNotExist:	
+                order_form = OrderForm()	
+        else:
+            order_form = OrderForm()
 
     if not stripe_public_key:
         messages.warning(request, 'Stripe public key is missing. \
