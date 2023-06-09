9fae0e1a07368e72c1e815735c0882b7f5994c03
Dmytro Martynenko <dmytromartynenko1@gmail.com>
2023-06-09 02:45:58 +0000

    Set Debug to False

--- claycafe/settings.py
+++ claycafe/settings.py
@@ -25,7 +25,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = False
+
+# 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [
     "claycafe.herokuapp.com", "localhost",
